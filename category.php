3841b00b69d9758ee3a81d0af4e0839dd38c6d29
Pedro Markun <pedro@markun.com.br>
2013-11-04 17:20:31 -0200

    adicionando check para sticky-category

---
+++ category.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying Category pages
+ *
+ * Used to display archive-type pages for posts in a category.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+			<!--
+			<header class="archive-header">
+				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentytwelve' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
+
+			<?php if ( category_description() ) : // Show an optional category description ?>
+				<div class="archive-meta"><?php echo category_description(); ?></div>
+			<?php endif; ?>
+			</header>
+			-->
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+				//Check if has category_sticky
+				$sticky_category = get_post_meta(  get_the_ID(), 'category_sticky_post', true );
+				if ( $sticky_category ) : 
+					get_template_part( 'content', get_post_format() ); 
+				else :
+					?>	
+					<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+				<?php 
+				endif;
+			endwhile;
+
+			twentytwelve_content_nav( 'nav-below' );
+			?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
