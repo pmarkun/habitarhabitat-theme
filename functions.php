b030f06464a76308102b53b7a18751907156c4df
Paulo Geyer <paulogeyer@gmail.com>
2013-11-05 00:29:44 -0200

    funcoes para pegar o thumbnail dos videos

--- functions.php
+++ functions.php
@@ -30,4 +30,69 @@ function render_video_youtube( $id, $w='720', $h='480' ) {
     printf($html);
 }
 
+function get_youtube_id($url) {
+    parse_str( parse_url( $url, PHP_URL_QUERY ), $my_array_of_vars );
+    $id = $my_array_of_vars['v'];
+    return $id;
+}
+
+function get_vimeo_id($url) {
+    $id = substr(parse_url($url, PHP_URL_PATH), 1);
+    return $id;
+}
+
+function get_video_thumbnail($url) {
+    $vimeo = strpos($url, "vimeo");
+    $youtube = strpos($url, "youtube");
+
+    if ( $youtube ) {
+        $url = 'http://img.youtube.com/vi/'. get_youtube_id($url) .'/0.jpg';
+    } elseif ( $vimeo ) {
+        $hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/". get_vimeo_id($url) .".php"));
+        $url = $hash[0]['thumbnail_medium'];  
+    }
+
+    return $url;
+}
+
+// Save Featured Image If It's a youtube video
+add_action('save_post', 'wds_video_sideload_post_thumb');
+function wds_video_sideload_post_thumb() {
+    global $post;
+
+    $video_url = get_post_meta( $post->ID, 'video_url', true );
+    $thumb_url = get_video_thumbnail($video_url);
+
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+    require_once(ABSPATH . 'wp-admin/includes/media.php');
+    set_time_limit(300);
+
+    if ( ! empty($thumb_url) ) {
+        // Download file to temp location
+        $tmp = download_url( $thumb_url );
+
+        // Set variables for storage
+        // fix file filename for query strings
+        preg_match('/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $thumb_url, $matches);
+        $file_array['name'] = basename($matches[0]);
+        $file_array['tmp_name'] = $tmp;
+
+        // If error storing temporarily, unlink
+        if ( is_wp_error( $tmp ) ) {
+            @unlink($file_array['tmp_name']);
+            $file_array['tmp_name'] = '';
+        }
+
+        // do the validation and storage stuff
+        $thumbid = media_handle_sideload( $file_array, $post->ID, $desc );
+        // If error storing permanently, unlink
+        if ( is_wp_error($thumbid) ) {
+            @unlink($file_array['tmp_name']);
+            return $thumbid;
+        }
+    }
+
+    set_post_thumbnail( $post, $thumbid );
+}
+
 ?>
