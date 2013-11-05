ae7da299af483753a23dcf93ee793e0b3307e60b
Pedro Markun <pedro@markun.com.br>
2013-11-05 15:30:09 -0200

    aumentando limite de upload

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php 
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
 
 //Adicionando MenU Header
 register_nav_menu( 'header', 'Header Menu' );
