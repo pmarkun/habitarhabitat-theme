7b4a2a29ea34e6bbde67f73ff6c7744bf2703d8b
Pedro Markun <pedro@markun.com.br>
2013-11-05 13:16:40 -0200

    comment link

--- content-link.php
+++ content-link.php
@@ -9,7 +9,6 @@
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header><?php _e( 'Link', 'twentytwelve' ); ?></header>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
 		</div><!-- .entry-content -->
