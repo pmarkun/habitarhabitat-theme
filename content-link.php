06e0e49544dcf0574dd8e2caaa03c83059ca8cb7
Pedro Markun <pedro@markun.com.br>
2013-11-05 13:20:27 -0200

    comment link

--- content-link.php
+++ content-link.php
@@ -10,6 +10,8 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="entry-content">
+            <h1 class="entry-title">Anexos</h1>
+
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
 		</div><!-- .entry-content -->
 	</article><!-- #post -->
