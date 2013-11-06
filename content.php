4daf562eb42184ec8d86b7002cb75a9463e37496
Pedro Markun <pedro@markun.com.br>
2013-11-06 12:39:23 -0200

    adicionando conteúdo padrão

--- content.php
+++ content.php
@@ -7,6 +7,16 @@
  * @since Twenty Twelve 1.0
  */
 ?>
+    <header class="entry-header">
+        <?php the_post_thumbnail(); ?>
+        <?php if ( is_single() ) : ?>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php else : ?>
+        <h1 class="entry-title">
+            <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+        </h1>
+        <?php endif; // is_single() ?>
+    </header><!-- .entry-header -->
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="entry-content">
