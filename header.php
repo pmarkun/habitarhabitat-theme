c646f865875cd149cceef3a5c61d42ad5d5ee214
Pedro Markun <pedro@markun.com.br>
2013-11-11 19:04:20 -0200

    mudanca no texto do menu

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<!-- <h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
 			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a> -->
-			<h2 class='nav-title'>Escolha um tema ao lado: </h2>
+			<h2 class='nav-title'>Escolha um habitat/habitar: </h2>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu-habitar' ) ); ?>
 		</nav><!-- #site-navigation -->
 
