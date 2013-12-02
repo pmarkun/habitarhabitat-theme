201a9b5ad3aefc04347c781db8695f3a4d22f824
Paulo Geyer <paulogeyer@gmail.com>
2013-12-02 02:11:22 -0200

    adicionando css e js do leaflet

--- header.php
+++ header.php
@@ -29,6 +29,14 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
+
+<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css" />
+<!--[if lte IE 8]>
+    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.ie.css" />
+<![endif]-->
+
+<script src="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js"></script>
+
 <?php wp_head(); ?>
 </head>
 
